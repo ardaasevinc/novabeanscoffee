5a613a9eb977086b346b3cb7a2535258b6bf85c2
ardaasevinc <ardaasevinc@gmail.com>
2025-12-29 15:56:51 +0300

    topbar is setting

--- resources/views/site/components/topbar.blade.php
+++ resources/views/site/components/topbar.blade.php
@@ -7,12 +7,12 @@
                         <li>
                             <a href="mailto:info@novakitchen.com.tr">
                                 <img src="{{ asset('assets/images/icon-mail.svg') }}" alt="Email">
-                                info@novakitchen.com.tr
+                                {{ $setting?->email }}
                             </a>
                         </li>
                         <li>
                             <img src="{{ asset('assets/images/icon-location.svg') }}" alt="Konum">
-                            Ferhatpaşa Mahallesi Mescit Sokak Çağlayan Ege Ticaret Merkezi No:1 Kat: 2
+                            {{ strip_tags($setting?->address) }}
                         </li>
                         </ul>
                         </div>
@@ -22,14 +22,14 @@
                 <div class="topbar-social-links">
                     <ul>
                         <li>
-                            <a href="https://instagram.com/novabeanscoffee" target="_blank">
+                            <a href="{{ $setting?->instagram_url }}" target="_blank">
                                 <i class="fa-brands fa-instagram"></i>
                             </a>
                         </li>
-                        {{--
-                        <li><a href="#"><i class="fa-brands fa-facebook-f"></i></a></li>
-                        <li><a href="#"><i class="fa-brands fa-dribbble"></i></a></li>
-                        --}}
+                        
+                        {{-- <li><a href="#"><i class="fa-brands fa-facebook-f"></i></a></li>
+                        <li><a href="#"><i class="fa-brands fa-dribbble"></i></a></li> --}}
+                       
                         </ul>
                         </div>
                 </div>
