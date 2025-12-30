b106e4dfbc7663200bfa9d792c6b8b4a9ca72d48
ardaasevinc <ardaasevinc@gmail.com>
2025-12-30 18:58:13 +0300

    meta_title

--- resources/views/site/components/topbar.blade.php
+++ resources/views/site/components/topbar.blade.php
@@ -20,44 +20,52 @@
 
             <div class="col-md-3">
                 <div class="topbar-social-links">
-                    <ul>
-                       
-                        @if($setting?->phone)
-                            <li>
-                                <a href="https://wa.me/{{ preg_replace('/\D/', '', $setting->phone) }}" target="_blank"
-                                    title="WhatsApp">
-                                    <i class="fa-brands fa-whatsapp"></i>
-                                </a>
-                            </li>
-                        @endif
+                   <ul>
+    {{-- WhatsApp: Sadece sayıları filtrelediği için protokol eklemeye gerek yok --}}
+    @if($setting?->phone)
+        <li>
+            <a href="https://wa.me/{{ preg_replace('/\D/', '', $setting->phone) }}" target="_blank" title="WhatsApp">
+                <i class="fa-brands fa-whatsapp"></i>
+            </a>
+        </li>
+    @endif
 
-                        
-                        @if($setting?->instagram_url)
-                            <li>
-                                <a href="{{ $setting->instagram_url }}" target="_blank" title="Instagram">
-                                    <i class="fa-brands fa-instagram"></i>
-                                </a>
-                            </li>
-                        @endif
+    {{-- Instagram --}}
+    @if($setting?->instagram_url)
+        <li>
+            @php
+                $instagramUrl = str_starts_with($setting->instagram_url, 'http') ? $setting->instagram_url : 'https://' . $setting->instagram_url;
+            @endphp
+            <a href="{{ $instagramUrl }}" target="_blank" title="Instagram">
+                <i class="fa-brands fa-instagram"></i>
+            </a>
+        </li>
+    @endif
 
-                        
-                        @if($setting?->facebook_url)
-                            <li>
-                                <a href="{{ $setting->facebook_url }}" target="_blank" title="Facebook">
-                                    <i class="fa-brands fa-facebook-f"></i>
-                                </a>
-                            </li>
-                        @endif
+    {{-- Facebook --}}
+    @if($setting?->facebook_url)
+        <li>
+            @php
+                $facebookUrl = str_starts_with($setting->facebook_url, 'http') ? $setting->facebook_url : 'https://' . $setting->facebook_url;
+            @endphp
+            <a href="{{ $facebookUrl }}" target="_blank" title="Facebook">
+                <i class="fa-brands fa-facebook-f"></i>
+            </a>
+        </li>
+    @endif
 
-                       
-                        @if($setting?->x_url)
-                            <li>
-                                <a href="{{ $setting->x_url }}" target="_blank" title="X">
-                                    <i class="fa-brands fa-x-twitter"></i>
-                                </a>
-                            </li>
-                        @endif
-                    </ul>
+    {{-- X (Twitter) --}}
+    @if($setting?->x_url)
+        <li>
+            @php
+                $xUrl = str_starts_with($setting->x_url, 'http') ? $setting->x_url : 'https://' . $setting->x_url;
+            @endphp
+            <a href="{{ $xUrl }}" target="_blank" title="X">
+                <i class="fa-brands fa-x-twitter"></i>
+            </a>
+        </li>
+    @endif
+</ul>
                 </div>
             </div>
         </div>
