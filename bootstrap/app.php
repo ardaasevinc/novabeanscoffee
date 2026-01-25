c902d46a478df2f105cb08cfcc86a9407b2e53a1
ardaasevinc <ardaasevinc@gmail.com>
2026-01-25 21:15:20 +0300

    honeypot middlewear

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->appendToGroup('web', \Spatie\Honeypot\ProtectAgainstSpam::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
